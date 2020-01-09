2ba333aa13e6c8bfa6ac58d7d7e7dbdb897a1ca6
davisjustinw <davisjustinw@gmail.com>
2020-01-08 20:23:21 -0800

    add line Class with add, drop methods and functioning tests

--- frontend/index.js
+++ frontend/index.js
@@ -3,38 +3,98 @@
 function getWords() {
   console.log('getting words')
   let words = [
-    { str: 'Alice', syl: 2 },
-    { str: 'was', syl: 1 },
-    { str: 'beginning', syl: 3 },
-    { str: 'to', syl: 1 },
-    { str: 'get', syl: 1 },
-    { str: 'very', syl: 2 },
-    { str: 'tired', syl: 2 },
-    { str: 'of', syl: 1 },
-    { str: 'sitting', syl: 2 },
-    { str: 'by', syl: 1 },
-    { str: 'her', syl: 1 },
-    { str: 'sister', syl: 2 },
-    { str: 'on', syl: 1 },
-    { str: 'the', syl: 1 },
-    { str: 'bank', syl: 1 }
+    { text: 'Alice', count: 2 },
+    { text: 'was', count: 1 },
+    { text: 'beginning', count: 3 },
+    { text: 'to', count: 1 },
+    { text: 'get', count: 1 },
+    { text: 'very', count: 2 },
+    { text: 'tired', count: 1 },
+    { text: 'of', count: 1 },
+    { text: 'sitting', count: 2 },
+    { text: 'by', count: 1 },
+    { text: 'her', count: 1 },
+    { text: 'sister', count: 2 },
+    { text: 'on', count: 1 },
+    { text: 'the', count: 1 },
+    { text: 'bank', count: 1 }
   ];
   return words;
 }
 
 class Word {
   constructor(obj) {
-    this.str = obj.str;
-    this.syl = obj.syl;
+    this.text = obj.text;
+    this.count = obj.count;
   }
 }
 
-function loadAndStart() {
+class Line {
+  // think about getter setters here to protect vairables?
+  constructor(syllableMax) {
+    this.max = syllableMax;
+    this.words = [];
+    this.count = 0;
+  }
+
+  add = function(word) {
+    if (word.count + this.count <= this.max ) {
+      this.words.push(word);
+      this.count += word.count;
+      return true;
+    } else {
+      return false;
+    }
+    // maybe return the word or the list not sure here
+  }
+
+  drop = function(wordIndex) {
+    let dropped = this.words.splice(wordIndex, 1)[0];
+    this.count -= dropped.count;
+    return dropped;
+  }
+}
+
+class Stanza {
+  constructor(structure) {
+    this.lines = structure.map( count => new Line(count))
+
+  }
+}
+
+function test() {
+  words = getWords();
+
+  word1 = new Word(words[0]);
+  word2 = new Word(words[2]);
+  word3 = new Word(words[6]);
+  word4 = new Word(words[7]);
+  word5 = new Word(words[11]);
+  console.log(`Alice's word class name: ${word1.constructor.name}`);
+  console.log(`Alice's word returns proper count of 2: ${word1.count == 2}`);
+
+  line1 = new Line(5);
+  line2 = new Line(7);
+  console.log(`line1 class name: ${line1.constructor.name}`);
+
+  console.log(`add a word to line with proper count: ${line1.add(word1)}`);
+  console.log(`line count after addition is 2: ${line1.count == 2}`);
+  console.log(`fill a line: ${line1.add(word2)}`);
+  console.log(`line count after addition is 5: ${line1.count == 5}`);
+  console.log(`reject any additional words: ${line1.add(word3) == false}`);
+  console.log(`drop a word by index and return correct word: ${line1.drop(1).text == 'beginning'}`);
+  console.log(`line after drop shows proper count: ${line1.count == 2}`);
+
+
+}
+
+function start() {
   console.log('loading');
   let wordData = getWords();
   let words = wordData.map(obj => new Word(obj));
+  let ku = new Stanza([5,7,5])
 
-  console.log(words[0]);
+
   console.log('starting');
 }
-window.addEventListener("DOMContentLoaded", loadAndStart(), false);
+window.addEventListener("DOMContentLoaded", test(), false);
