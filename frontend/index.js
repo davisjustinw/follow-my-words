dbb66ac2c4c1db4f88610f2bafb7cc9a945354fc
davisjustinw <davisjustinw@gmail.com>
2020-01-11 20:58:18 -0800

    first-commit broken

--- frontend/index.js
+++ frontend/index.js
@@ -3,21 +3,21 @@
 function getWords() {
   console.log('getting words')
   let words = [
-    { text: 'Alice', count: 2, id: 1 },
-    { text: 'was', count: 1, id: 2 },
-    { text: 'beginning', count: 3, id: 3 },
-    { text: 'to', count: 1, id: 4 },
-    { text: 'get', count: 1, id: 5 },
-    { text: 'very', count: 2, id: 6 },
-    { text: 'tired', count: 1, id: 7 },
-    { text: 'of', count: 1, id: 8 },
-    { text: 'sitting', count: 2, id: 9 },
-    { text: 'by', count: 1, id: 10 },
-    { text: 'her', count: 1, id: 11 },
-    { text: 'sister', count: 2, id: 12 },
-    { text: 'on', count: 1, id: 13 },
-    { text: 'the', count: 1, id: 14 },
-    { text: 'bank', count: 1, id: 15 }
+    { text: 'Alice', count: 2, id: 'word-1' },
+    { text: 'was', count: 1, id: 'word-2' },
+    { text: 'beginning', count: 3, id: 'word-3' },
+    { text: 'to', count: 1, id: 'word-4' },
+    { text: 'get', count: 1, id: 'word-5' },
+    { text: 'very', count: 2, id: 'word-6' },
+    { text: 'tired', count: 1, id: 'word-7' },
+    { text: 'of', count: 1, id: 'word-8' },
+    { text: 'sitting', count: 2, id: 'word-9' },
+    { text: 'by', count: 1, id: 'word-10' },
+    { text: 'her', count: 1, id: 'word-11' },
+    { text: 'sister', count: 2, id: 'word-12' },
+    { text: 'on', count: 1, id: 'word-13' },
+    { text: 'the', count: 1, id: 'word-14' },
+    { text: 'bank', count: 1, id: 'word-15' }
   ];
   return words;
 }
@@ -30,7 +30,7 @@ class Word {
     this.count = obj.count;
     this.element = document.createElement('span');
     this.element.innerText = `${this.text} `;
-    this.element.setAttribute('id', `word${this.id}`);
+    this.element.setAttribute('id', this.id);
   }
 }
 
@@ -38,16 +38,21 @@ class Word {
 class Line {
   constructor(syllableMax) {
     this.max = syllableMax;
-    this.words = [];
+    this.words = {};
     this.count = 0;
+    this.complete = false;
     this.element = document.createElement('p');
   }
 
   add = function(word) {
     if (word.count + this.count <= this.max ) {
-      this.words.push(word);
+      this.words[word.id] = word;
       this.count += word.count;
+
+      if (this.count == this.max) { this.complete == true };
+
       this.element.appendChild(word.element);
+      console.log(this.words);
       return true;
     } else {
       return false;
@@ -55,8 +60,12 @@ class Line {
     // maybe return the word or the list not sure here
   }
 
-  drop = function(wordIndex) {
-    let dropped = this.words.splice(wordIndex, 1)[0];
+  drop = function(wordId) {
+    console.log(`drop ${wordId}`)
+    let dropped = this.words[wordId];
+    console.log(this.words);
+    console.log(dropped);
+    delete this.words[dropped];
     this.count -= dropped.count;
     this.element.removeChild(dropped.element);
     return dropped;
@@ -67,11 +76,14 @@ class Line {
 class Stanza {
   constructor(structure, element) {
     this.element = element;
+
     this.lines = structure.map(count => {
       let newLine = new Line(count);
       this.element.appendChild(newLine.element);
       return newLine;
     });
+    this.currentLine = this.lines[0];
+
   }
 
   length = function() {
@@ -85,11 +97,17 @@ class Board {
     this.elQueue = document.querySelector('#words ul');
     this.elCurrentStanza = document.querySelector('#current-stanza');
     this.elSavedStanza = document.querySelector('#saved-stanza');
+    this.currentStanza = new Stanza([5,7,5], this.elCurrentStanza);
 
     this.elQueue.addEventListener('click', e => {
       e.stopPropagation();
       e.currentTarget.removeChild(e.target);
       this.elCurrentStanza.appendChild(e.target);
+      let id = e.target.getAttribute('id');
+      let word = this.words[id];
+      console.log(this.currentStanza.currentLine);
+      this.currentStanza.currentLine.add(word);
+      console.log(this.currentStanza.currentLine);
     }, true);
 
     this.words = getWords().reduce((words, obj) => {
@@ -99,7 +117,20 @@ class Board {
       return words;
     }, {});
 
-    this.currentStanza = new Stanza([5,7,5], this.elCurrentStanza);
+    this.currentStanza.element.addEventListener('click', e => {
+
+      let pNode = e.target.parentNode.firstChild;
+      e.target.parentNode.removeChild(pNode);
+      let id = e.target.getAttribute('id');
+      let dropped = this.currentStanza.currentLine.drop(id);
+      console.log('dropped');
+      console.log(dropped);
+      console.log('end');
+      this.words[dropped.id] = dropped
+      this.elQueue.appendChild(dropped.element);
+      // return dropped to the queue;
+    }, true);
+
   }
 
   saveStanza = function() {
@@ -114,15 +145,10 @@ class Board {
 
 }
 
-
-
-
-// stanzaListener
-
 function start() {
   test()
   console.log('loading');
-  //build gameboard
+
   let board = new Board();
 
   console.log('starting');
@@ -150,7 +176,7 @@ function test() {
   console.log(`fill a line: ${line1.add(word2)}`);
   console.log(`line count after addition is 5: ${line1.count == 5}`);
   console.log(`reject any additional words: ${line1.add(word3) == false}`);
-  console.log(`drop a word by index and return correct word: ${line1.drop(1).text == 'beginning'}`);
+  console.log(`drop a word by index and return correct word: ${line1.drop('word-1').text == 'beginning'}`);
   console.log(`line after drop shows proper count: ${line1.count == 2}`);
 
 }
