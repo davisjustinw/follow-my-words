5be7791fe85d9ca0324f8eeb35562ddbcaacf30c
davisjustinw <davisjustinw@gmail.com>
2020-01-12 22:25:25 -0800

    add words route with limit and count query

--- backend/app/controllers/words_controller.rb
+++ backend/app/controllers/words_controller.rb
@@ -1,6 +1,8 @@
 class WordsController < ApplicationController
   def index
-    words = Word.all
+    count = 10
+    count = params[:count].to_i if params[:count]
+    words = Word.limit(count)
     render json: words, only: [:id, :text, :count]
   end
 end
